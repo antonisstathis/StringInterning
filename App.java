cdf199b2c46aebfe374eab97513f9e7cd3e758dc
antonisstathis <antonisstathis95@gmail.com>
2023-01-29 10:32:01 +0200

    Update App.java

--- App.java
+++ App.java
@@ -5,7 +5,12 @@ public class App {
 		String s1 = "hello";
 		String s2 = "hello";
 
-		// We compare references here which are pointers actually and when we run it we notice that the references s1 and s2 point to the same object in the heap. The jvm is clever enough to know that we try to save the same object in the heap and that's why it just makes the s2 reference to point to the same object in the heap and doesn't waste memory from the heap to save the same information again. 
+		/*
+		We compare references here which are pointers actually and when we run it we notice that the references s1 and s2 point 
+		to the same object in the heap. The jvm is clever enough to know that we try to save the same object in the heap and that's 
+		why it just makes the s2 reference to point to the same object in the heap and doesn't waste memory from the heap to save the 
+		same information again.
+		*/
 		if (s1 == s2) {
 			print("They are the same objets.");
 		}
@@ -13,7 +18,13 @@ public class App {
 			print("They are different objets.");
 		}
 
-		// While here we notice that they are different objects despite the fact that the same information is saved in the heap. It means that here the jvm allocated memory to save again the same information and saved the memory location of the second object created to the s4 reference in the stack. In the first case we had only one object in the heap and two references (s1 and s2) in the stack to point to this single object and in this second case we have two different objects saved in the heap and two different references (s3 and s4) saved in the stack to point to these different objects.
+		/*
+		While here we notice that they are different objects despite the fact that the same information is saved in the heap. It means that
+		here the jvm allocated memory to save again the same information and saved the memory location of the second object created to the 
+		s4 reference in the stack. In the first case we had only one object in the heap and two references (s1 and s2) in the stack to point 
+		to this single object and in this second case we have two different objects saved in the heap and two different references (s3 and s4) 
+		saved in the stack to point to these different objects.
+		*/
 		String s3 = new Integer(5).toString();
 		String s4 = "5";
 		if (s3 == s4) {
@@ -23,7 +34,11 @@ public class App {
 			print("They are different objets.");
 		}
 
-		// If we want to reuse the object in case it is the same information to be saved many times (and not duplicates be created and waste memory from the heap) we have to call the method intern(). Here we notice that the condition s5 == s6 is true and the first message is printed in the screen declaring that the 2 references point to the same object in the heap.
+		/*
+		If we want to reuse the object in case it is the same information to be saved many times (and not duplicates be created and waste 
+		memory from the heap) we have to call the method intern(). Here we notice that the condition s5 == s6 is true and the first message 
+		is printed in the screen declaring that the 2 references point to the same object in the heap.
+		*/
 		String s5 = new Integer(5).toString().intern();
 		String s6 = "5";
 		if (s5 == s6) {
